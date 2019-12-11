1b0aa096dfac521a4a4b0aeaa64577062bba0bf1
chamathabeysinghe <abeysinghechamath@gmail.com>
2019-12-11 15:01:16 +1100

    Support multiple video files

--- data/videos/convert_data.py
+++ data/videos/convert_data.py
@@ -1,30 +1,49 @@
+"""
+Convert VitBat CSV file as we need.
+"""
+
 import pandas as pd
-filepath = 'test_coordinates.txt'
-lines = []
-
-frames_count = 0
-rois_per_frame = 0
-
-with open(filepath) as fp:
-    for cnt, line in enumerate(fp):
-        if cnt < 3:
-            continue
-        line = [int(round(float(x))) for x in line.strip('\n').split('\t')]
-
-        rois_per_frame = max(rois_per_frame, line[1])
-        frames_count = line[0]
-        lines.append(line)
+import glob
+import os
 
 
-data_array = [[[-1, -1, -1, -1] for _ in range(rois_per_frame)] for _ in range(frames_count)]
+def convert_csv(read_path, save_path):
+    lines = []
 
-for line in lines:
-    frame_index = line[0]
-    roi_index = line[1]
-    square = [line[3], line[2], line[3] + line[5], line[2] + line[4]]
-    data_array[frame_index - 1][roi_index - 1] = square
+    frames_count = 0
+    rois_per_frame = 0
 
-df = pd.DataFrame(data_array)
-print(df.head())
-df.to_csv('frames_test/data.csv')
+    with open(read_path) as fp:
+        for cnt, line in enumerate(fp):
+            if cnt < 3:
+                continue
+            line = [int(round(float(x))) for x in line.strip('\n').split('\t')]
+
+            rois_per_frame = max(rois_per_frame, line[1])
+            frames_count = line[0]
+            lines.append(line)
+
+    data_array = [[[-1, -1, -1, -1] for _ in range(rois_per_frame)] for _ in range(frames_count)]
+
+    for line in lines:
+        frame_index = line[0]
+        roi_index = line[1]
+        square = [line[3], line[2], line[3] + line[5], line[2] + line[4]]
+        data_array[frame_index - 1][roi_index - 1] = square
+
+    df = pd.DataFrame(data_array)
+    print(df.head())
+    df.to_csv(save_path)
+
+
+files = sorted(glob.glob('./annotations/out_*_IndividualStates.txt'))
+indexes = [int(x[x.index('_')+1:x.rfind('_')]) for x in files]
+
+for x in zip(indexes, files):
+    print(x)
+    save_path = './frames/{}/data.csv'.format(x[0])
+    # os.makedirs(save_path, exist_ok=True)
+    read_path = x[1]
+    convert_csv(read_path, save_path)
+
 
--- data/videos/convert_video_to_images.py
+++ data/videos/convert_video_to_images.py
@@ -1,9 +1,28 @@
+"""
+Extract frames from videos and save as images
+"""
+
 import cv2
-vidcap = cv2.VideoCapture('out_test.mp4')
-success, image = vidcap.read()
-count = 0
-while success:
-    cv2.imwrite("frames_test/frame_%d.png" % count, image)     # save frame as JPEG file
+import os
+import glob
+
+
+def convert_video_to_frames(read_path, save_path):
+    vidcap = cv2.VideoCapture(read_path)
     success, image = vidcap.read()
-    print('Read a new frame: ', success)
-    count += 1
+    count = 0
+    while success:
+        cv2.imwrite("{}/frame_{}.png".format(save_path, count), image)     # save frame as JPEG file
+        success, image = vidcap.read()
+        print('Read a new frame: ', success)
+        count += 1
+
+
+files = sorted(glob.glob('./annotations/out_*.mp4'))
+indexes = [int(x[x.rfind('_')+1:x.rfind('.')]) for x in files]
+
+for x in zip(indexes, files):
+    save_path = './frames/{}'.format(x[0])
+    os.makedirs(save_path, exist_ok=True)
+    read_path = x[1]
+    convert_video_to_frames(read_path, save_path)
